7af2fec932d48d55a6537d4a66373fd3473852a5
lirui <634178463@qq.com>
2019-02-11 22:19:36 +0800

    通道

---
+++ go-geek/demo20.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// 通道
+func main() {
+	// 声明通道。容量为 3
+	ch1 := make(chan int, 3)
+
+	ch1 <- 2
+	ch1 <- 1
+	ch1 <- 3
+
+	elem1 := <- ch1
+	elem2 := <- ch1
+	elem3 := <- ch1
+
+	fmt.Println(elem1)
+	fmt.Println(elem2)
+	fmt.Println(elem3)
+}
